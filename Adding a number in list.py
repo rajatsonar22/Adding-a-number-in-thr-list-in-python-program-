6625b54b2d5ea8f8d73c1afc1b4b2adfa545a691
RAJAT SONAR <150504303+rajatsonar22@users.noreply.github.com>
2023-12-20 11:12:06 +0530

    Add files via upload

---
+++ Adding a number in list.py
@@ -0,0 +1,8 @@
+#adding a number in list
+#Adding the number in the list using append function
+list = [1,2,3,4,5]
+print("---this will add the numbers in the end ")
+add=int(input("Enter a number"))
+list.append(add)
+print(" >>> The required result is :",list)
+print(" ")#...space
